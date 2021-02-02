b125d04fd8d40b9b5122bebd0f7513aca426bea1
Takahiro Taniguchi <wtaka95@gmail.com>
2021-02-02 20:40:47 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+var corsOptions = {
+  origin: 'https://localhost:8081'
+};
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', (req, res) => {
+  res.json({message: 'Welcome to Takahiro application'});
+})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
